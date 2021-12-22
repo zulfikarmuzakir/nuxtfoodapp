f227b08f8e2ee023aa0a22e405c428bbe575c6b4
Zulfikar Ali Muzakir <zulfikar26error@gmail.com>
2021-12-22 23:54:23 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Nuxt />
+    </div>
+</template>
+
+<style>
+    html {
+        font-family: 
+        'Mulish',
+        'Source Sans Pro',
+        -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-weight: 300;
+        font-size: 16px;
+        word-spacing: 1px;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        box-sizing: border-box;
+    }
+
+    h1, h2, h3, h4 {
+        font-family: 
+        'Poppins',
+        -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-weight: 600;
+    }
+</style>
